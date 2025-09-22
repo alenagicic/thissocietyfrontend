1bcb76e404dbe283517fee5f0fc82c42426c4b23
alenagicic <alenagicic@gmail.com>
2025-09-22 06:56:35 +0200

    Redeploying bugfix.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { AuthContext } from '../context/AuthContext';
+import { AuthContext } from '../Context/AuthContext';
 import { apiCall } from '../Utils/api';
 
 export default function Header() {
